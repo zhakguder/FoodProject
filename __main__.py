0788b480d25ccce4fbf3d59ef1950d145223b61f
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-10 22:17:43 -0600

    minor

--- __main__.py
+++ __main__.py
@@ -26,7 +26,7 @@ recipes = get_recipe_from_db(277888)
 for image in recipes['images']:
     preds = image_classification_model.get_ingredients(image)
     preds = [x.strip() for x in preds.split(',')]
-
+    print(preds)
 
 # print(recipe_ids())
 # sim_ctrl = SimilarityController()
