d639d2043c8d2be60d56e4df470180acd41e77a3
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-11 16:39:38 -0600

    minor

--- food_project/image_classification/ingredient_predictor.py
+++ food_project/image_classification/ingredient_predictor.py
@@ -14,16 +14,6 @@ prefix = '/FoodProject/data/image_classification/model/'
 model = tf.keras.models.load_model(prefix+'hyvee.best.hdf5')
 logits = model.predict(images_for_prediction)
 
-# partition = 'validation'
-# path = partition + '_ds'
-# spec = (tf.TensorSpec(shape=(None, 256, 256, 3), dtype=tf.float32, name=None), tf.TensorSpec(shape=(None,), dtype=tf.int32, name=None))
-
-# val_data = tf.data.experimental.load(path, spec)
-
-
-# val_data_x = val_data.map(lambda x, y: x)
-# val_data_y = val_data.map(lambda x, y: y)
-# preds = trained_model.predict(val_data_x)
 
 predicted_class_indexes = tf.argmax(tf.nn.softmax(logits), axis=1)
 
@@ -31,6 +21,5 @@ with open(prefix+'hyvee_label.dict', 'rb') as f:
     mapping = pickle.load(f)
 
 reverse_map = {v:k for k,v in mapping.items()}
-breakpoint()
-predicted_classes = [reverse_map[x] for x in predicted_class_indexes]
+predicted_classes = [reverse_map[x] for x in predicted_class_indexes.numpy()]
 print(predicted_classes)
