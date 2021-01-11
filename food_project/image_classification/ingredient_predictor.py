e1e4fa58f1d24b3524bf6b57ca4286af6cc2c96b
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-11 16:35:45 -0600

    minor

--- food_project/image_classification/ingredient_predictor.py
+++ food_project/image_classification/ingredient_predictor.py
@@ -9,7 +9,9 @@ from PIL import Image
 image_paths = argv[1:]
 f = Image.open
 images_for_prediction = np.stack([f(x) for x in image_paths])
-model = tf.keras.models.load_model('/FoodProject/data/image_classification/model/hyvee.best.hdf5')
+prefix = '/FoodProject/data/image_classification/model/'
+
+model = tf.keras.models.load_model(prefix+'hyvee.best.hdf5')
 logits = model.predict(images_for_prediction)
 
 # partition = 'validation'
@@ -25,7 +27,7 @@ logits = model.predict(images_for_prediction)
 
 predicted_class_indexes = tf.argmax(tf.nn.softmax(logits), axis=1)
 
-with open('hyvee_label.dict', 'rb') as f:
+with open(prefix+'hyvee_label.dict', 'rb') as f:
     mapping = pickle.load(f)
 
 reverse_map = {v:k for k,v in mapping.items()}
