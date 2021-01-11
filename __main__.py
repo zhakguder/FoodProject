c50b2dc52db98e3ad171736c34616efe51ca564b
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-10 22:23:02 -0600

    minor

--- __main__.py
+++ __main__.py
@@ -33,7 +33,7 @@ for image in recipes['images']:
     preds = image_classification_model.get_ingredients(image)
     preds = [x.strip() for x in preds.split(',')]
     res = sim_ctrl.handle(preds, 10)
-    print(res.index)
+    print([int(x) for x in res.index.values])
 
 
 # print(recipe_ids())
