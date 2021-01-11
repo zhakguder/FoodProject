9fe82867de1b9464d841c402b8af4d49844bd282
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-10 23:00:33 -0600

    minor

--- food_project/util.py
+++ food_project/util.py
@@ -12,30 +12,30 @@ def read_json(path):
         content =  json.load(f)
     return content
 
-    def read_pickle(path):
-        with open(path, 'rb') as f:
-            content = pickle.load(f)
-        return content
-    def column_name(df, i):
-        return df.columns[i]
+def read_pickle(path):
+    with open(path, 'rb') as f:
+        content = pickle.load(f)
+    return content
+def column_name(df, i):
+    return df.columns[i]
 
-    def column_value(df, i):
-        return df.iloc[:, i]
+def column_value(df, i):
+    return df.iloc[:, i]
 
-    def dataframe_from_dict(mydict):
-        return pd.DataFrame(mydict)
+def dataframe_from_dict(mydict):
+    return pd.DataFrame(mydict)
 
-    def matching_columns(df, text):
-        return df.columns.str.contains(text)
+def matching_columns(df, text):
+    return df.columns.str.contains(text)
 
-    def _content_matches(path, match_cond):
-        return match_cond.match(path)
+def _content_matches(path, match_cond):
+    return match_cond.match(path)
 
-    def list_content_with_matches(path, match_cond):
-        contents = os.listdir(path)
-        def abs_path(x):
-            return os.path.join(path, x)
-        return  [abs_path(x) for x in contents if match_cond.match(abs_path(x))]
+def list_content_with_matches(path, match_cond):
+    contents = os.listdir(path)
+    def abs_path(x):
+        return os.path.join(path, x)
+    return  [abs_path(x) for x in contents if match_cond.match(abs_path(x))]
 
 class FilesystemMatch:
     def __init__(self, fn, *args):
