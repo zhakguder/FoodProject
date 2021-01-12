1bd3871433c304bfa430985ddc4c243cd5880151
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-11 20:40:05 -0600

    server

---
+++ food_project/image_classification/ingredient_prediction_server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import pickle
+from sys import argv
+from PIL import Image
+from flask import Flask, request, Response
+app = Flask(__name__)
+
+
+# image_paths = argv[1:]
+
+# f = Image.open
+
+images_for_prediction = np.stack([f(x) for x in image_paths])
+prefix = "/FoodProject/data/image_classification/model/"
+
+model = tf.keras.models.load_model(prefix + "hyvee.best.hdf5")
+with open(prefix + "hyvee_label.dict", "rb") as f:
+    mapping = pickle.load(f)
+
+reverse_map = {v: k for k, v in mapping.items()}
+
+
+def predict_class_indexes(np_arr):
+    logits = model.predict(np_arr)
+    return tf.argmax(tf.nn.softmax(logits), axis=1)
+
+def predict_class_labels(arr):
+    indexes = predict_class_indexes(arr).numpy()
+    return [reverse_map[x].replace('_', ' ') for x in indexes]
+
+
+@app.route('/predict-label/', methods=['POST'])
+@limit_content_length(1000 * 1024 * 1024)
+def predict():
+    image = request.files['image']
+    prediction = predict_class_labels(image)
+    result = parse.unquote_plus(prediction)
+    return Response(result, status=200)
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port='8888')
--- food_project/image_classification/models.py
+++ food_project/image_classification/models.py
@@ -2,19 +2,22 @@
 import requests
 import os
 
+
 class DishImageClassificationModel:
     def _init__(self):
         self.uri = None
+
     def get_ingredients(self, img_path):
         with open(img_path, "rb") as img:
             img_name = os.path.basename(img_path)
-            files = {'image': (img_name, img, "multipart/form-data", {"Expires": "0"})}
+            files = {"image": (img_name, img, "multipart/form-data", {"Expires": "0"})}
             with requests.Session() as s:
                 r = s.post(self.uri, files=files)
         return r.text
 
     def accept(self, visitor):
         visitor.visit(self)
+
 
 class ImageClassificationModelInitiator:
     def __init__(self, uri, port, route):
@@ -23,4 +26,8 @@ class ImageClassificationModelInitiator:
     def visit(self, element):
         element.uri = self.uri
 
-image_classification_models = {'dish': DishImageClassificationModel(), 'ingredient': IngredientImageClassificationModel()}
+
+image_classification_models = {
+    "dish": DishImageClassificationModel(),
+    "ingredient": IngredientImageClassificationModel(),
+}
