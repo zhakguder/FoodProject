149ddb517f81a706b7d3a2d043de16adf5448b03
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-11 22:11:47 -0600

    minor

--- food_project/image_classification/ingredient_prediction_server.py
+++ food_project/image_classification/ingredient_prediction_server.py
@@ -8,14 +8,10 @@ from PIL import Image
 from flask import Flask, request, Response
 from urllib import parse
 from functools import wraps
+
 app = Flask(__name__)
 
 
-# image_paths = argv[1:]
-
-# f = Image.open
-
-# images_for_prediction = np.stack([f(x) for x in image_paths])
 prefix = "/FoodProject/data/image_classification/model/"
 
 model = tf.keras.models.load_model(prefix + "hyvee.best.hdf5")
@@ -29,9 +25,11 @@ def predict_class_indexes(np_arr):
     logits = model.predict(np_arr)
     return tf.argmax(tf.nn.softmax(logits), axis=1)
 
+
 def predict_class_labels(arr):
     indexes = predict_class_indexes(arr).numpy()
     return [reverse_map[x] for x in indexes]
+
 
 def limit_content_length(max_length):
     def decorator(f):
@@ -41,20 +39,23 @@ def limit_content_length(max_length):
             if cl is not None and cl > max_length:
                 abort(413)
             return f(*args, **kwargs)
+
         return wrapper
+
     return decorator
 
 
-@app.route('/predict-label/', methods=['POST'])
+@app.route("/predict-label/", methods=["POST"])
 @limit_content_length(1000 * 1024 * 1024)
 def predict():
-    image = request.files['image']
+    image = request.files["image"]
     image = np.array(Image.open(image), dtype=float)
     shp = image.shape
-    image = image.reshape(1, *shp)/255
+    image = image.reshape(1, *shp) / 255
     predictions = predict_class_labels(image)
     result = [parse.unquote_plus(x) for x in predictions]
     return Response(result, status=200)
 
+
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port='8888')
+    app.run(debug=False, host="0.0.0.0", port="8888")
