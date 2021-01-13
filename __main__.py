627daa238267303ca4ea901782e121eeaf8b19a4
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-12 21:57:24 -0600

    predict for all dish images

--- __main__.py
+++ __main__.py
@@ -57,6 +57,7 @@ for recipe_id in get_recipe_ids_from_db():
         res = sim_ctrl.handle(preds, n_most_similar_recipes)
         recipe_ids = [int(x) for x in res.index.values]
         print(recipe_ids)
+        break
 # recipe = get_recipe_from_db(277888)
 # print(recipe['name'])
 # print(recipe['processed_ingredients'])
