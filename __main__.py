efe76000221d4c7334607a6fa6360d570bb8c5cd
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-12 22:39:40 -0600

    predict for all dish images

--- __main__.py
+++ __main__.py
@@ -53,7 +53,7 @@ processed_data_dir = "data/recipes/processed"
 all_recipe_ids = get_recipe_ids_from_db()
 hits = []
 random_ids = choices(all_recipe_ids, k=10)
-for recipe_id in all_recipe_ids:
+for recipe_id in random_ids:
     hit = []
     recipe = get_recipe_from_db(int(recipe_id))
     recipe_ingredients = recipe.get('processed_ingredients', '')
