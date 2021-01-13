25e610f7a63b7b1fefe005d13336d2908a2d7670
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-12 20:34:06 -0600

    minor

--- __main__.py
+++ __main__.py
@@ -32,25 +32,31 @@ sim_ctrl = SimilarityController()
 sim_ctrl_vis = SimilarityControllerVisitor(match_score)
 sim_ctrl_vis.visit(sim_ctrl)
 
-# populate_db_recipes('data/recipes/raw/ArgentinianRecipes')
-# populate_db_images('data/recipes/raw/ArgentinianRecipes')
-# populate_db_processed('data/recipes/processed/ArgentinianRecipes')
-recipe = get_recipe_from_db(277888)
-print(recipe['name'])
-print(recipe['processed_ingredients'])
-print("PREDICTIONS")
-for image in recipe['images']:
-    preds = dish_image_classification_model.get_ingredients(image)
-    preds = [x.strip() for x in preds.split(',')]
-    res = sim_ctrl.handle(preds, n_most_similar_recipes)
-    recipe_ids = [int(x) for x in res.index.values]
-    for recipe_id in recipe_ids:
-        try:
-            recipe = get_recipe_from_db(recipe_id)
-            print(recipe['name'])
-            print(recipe['processed_ingredients'])
-        except:
-            pass
+raw_data_dir = 'data/recipes/raw'
+processed_data_dir = 'data/recipes/processed'
+
+for directory in os.listdir(raw_data_dir):
+
+    populate_db_recipes(os.path.join(raw_data_dir, 'ArgentinianRecipes'))
+    populate_db_images(os.path.join(raw_data_dir, 'ArgentinianRecipes'))
+    populate_db_processed(os.path.join(processed_data_dir, 'data/recipes/processed/ArgentinianRecipes'))
+
+# recipe = get_recipe_from_db(277888)
+# print(recipe['name'])
+# print(recipe['processed_ingredients'])
+# print("PREDICTIONS")
+# for image in recipe['images']:
+#     preds = dish_image_classification_model.get_ingredients(image)
+#     preds = [x.strip() for x in preds.split(',')]
+#     res = sim_ctrl.handle(preds, n_most_similar_recipes)
+#     recipe_ids = [int(x) for x in res.index.values]
+#     for recipe_id in recipe_ids:
+#         try:
+#             recipe = get_recipe_from_db(recipe_id)
+#             print(recipe['name'])
+#             print(recipe['processed_ingredients'])
+#         except:
+#             pass
 
 # To copy processed recipes from HCC to pandas
 #find  ArgentinianRecipes -type f -regextype sed -regex ".*[0-9]\.out" -exec scp '{}' zeynep@sbbi-panda.unl.edu:/home/zeynep/projects/FoodProject/backend/data/processed/ArgentinianRecipes ';'
