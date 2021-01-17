36baa5d7ef6216399d260d54d34c60176ab09e16
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-01-17 14:20:43 -0600

    add processed recipes

--- __main__.py
+++ __main__.py
@@ -45,36 +45,37 @@ sim_ctrl_vis.visit(sim_ctrl)
 raw_data_dir = "data/recipes/raw"
 processed_data_dir = "data/recipes/processed"
 
-# for directory in os.listdir(raw_data_dir):
+for directory in os.listdir(raw_data_dir):
 #     populate_db_recipes(os.path.join(raw_data_dir, directory))
 #     populate_db_images(os.path.join(raw_data_dir, directory))
-#     populate_db_processed(os.path.join(processed_data_dir, directory))
+    populate_db_processed(os.path.join(processed_data_dir, directory))
 
-all_recipe_ids = get_recipe_ids_from_db()
-hits = []
-random_ids = choices(all_recipe_ids, k=10)
-for recipe_id in random_ids:
-    hit = []
-    recipe = get_recipe_from_db(int(recipe_id))
-    recipe_ingredients = recipe.get('processed_ingredients', '')
-    print("*"*10)
-    print(recipe_ingredients)
-    print("*"*10)
-    for image in recipe["images"]:
-        preds = image_classification_model.get_ingredients(image)
-        print('-'*10)
-        print(preds)
-        print('-'*10)
-        res = sim_ctrl.handle(preds, n_most_similar_recipes)
-        recipe_ids = [int(x) for x in res.index.values]
-        for id_ in recipe_ids:
-            print(get_recipe_from_db(id_).get('processed_ingredients', []))
-        hit.append(int(recipe_id) in recipe_ids)
-    print('='*20)
+# all_recipe_ids = get_recipe_ids_from_db()
+# hits = []
+# random_ids = choices(all_recipe_ids, k=10)
+# for recipe_id in random_ids:
+#     hit = []
+#     breakpoint()
+#     recipe = get_recipe_from_db(int(recipe_id))
+#     recipe_ingredients = recipe.get('processed_ingredients', '')
+#     print("*"*10)
+#     print(recipe_ingredients)
+#     print("*"*10)
+#     for image in recipe["images"]:
+#         preds = image_classification_model.get_ingredients(image)
+#         print('-'*10)
+#         print(preds)
+#         print('-'*10)
+#         res = sim_ctrl.handle(preds, n_most_similar_recipes)
+#         recipe_ids = [int(x) for x in res.index.values]
+#         for id_ in recipe_ids:
+#             print(get_recipe_from_db(id_).get('processed_ingredients', []))
+#         hit.append(int(recipe_id) in recipe_ids)
+#     print('='*20)
 
 
-    hits.append(hit)
-print(hits)
+#     hits.append(hit)
+# print(hits)
 # recipe = get_recipe_from_db(277888)
 # print(recipe['name'])
 # print(recipe['processed_ingredients'])
