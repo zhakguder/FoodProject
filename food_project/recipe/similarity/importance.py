d408c71953db8d15ca54f9a27215801db65f30a3
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-01 12:58:42 -0600

    importance interface

---
+++ food_project/recipe/similarity/importance.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from abc import ABCMeta, abstractmethod
+
+class ImportanceCalculator:
+    def _create_ranker(self):
+        '''Return an importance ranker'''
+        return EntropyRanker()
+    def calculate_importance(self, mask, mask_type):
+        ranker = self._create_ranker(mask_type)
+        return ranker.get_ranks(mask)
+
+class Ranker(ABCMeta):
+    @abstractmethod
+    def get_ranks(self, mask):
+        pass
+
+class EntropyRanker(Ranker):
+    def get_ranks(self, mask):
+        breakpoint()
