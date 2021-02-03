508fff02795a60271bd3c922481f16d3bf7f1420
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-02 18:41:09 -0600

    minor

--- food_project/recipe/__init__.py
+++ food_project/recipe/__init__.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 from food_project.recipe.flat_to_db import populate_db_recipes, populate_db_images, populate_db_processed
 from food_project.recipe.db_to_local import get_recipe_from_db, get_recipe_ids_from_db, get_processed_ingredients_from_db
-from food_project.recipe.models import RecipeDBInitiator, RecipeFilePathSetter,  RawRecipeGroup, raw_recipe_model, RecipeClusterModel
+from food_project.recipe.models import RecipeDBInitiator, RecipeFilePathSetter,  RawRecipeGroup, raw_recipe_model
+from food_project.recipe.models import get_recipe_cluster_model
 from food_project.recipe.similarity.controller import SimilarityController, SimilarityControllerVisitor
 import os
 import pandas as pd
+
+
+
 
 def connect_to_database(uri, uname, pwd):
     rdi = RecipeDBInitiator(uri, uname, pwd)
@@ -12,7 +16,7 @@ def connect_to_database(uri, uname, pwd):
 
 def save_cluster_df(path):
     if  not os.path.exists(path):
-        rcm = RecipeClusterModel()
+        rcm = get_recipe_cluster_model()
         rcm.export_cluster_df(path)
     else:
         print('File already exists!')
