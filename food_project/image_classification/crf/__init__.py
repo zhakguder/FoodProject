3dd096ef078ec50de0b96c63b30963f9b86eb226
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-07 18:51:48 -0600

    edge potentials

--- food_project/image_classification/crf/__init__.py
+++ food_project/image_classification/crf/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from food_project.image_classification.crf.potentials import EdgePotentials
+from food_project.image_classification.crf.test import make_test_image
 
-edge_potentials = EdgePotentials('data/image_classification/edge_potentials_dict.pkl')
+edge_potentials = EdgePotentials('data/crf/edge_potentials_dict.pkl')
 edge_potentials_dict = edge_potentials.get_frequencies()
