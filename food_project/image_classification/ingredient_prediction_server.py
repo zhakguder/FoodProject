e52a5340b345916461b005cb58b3ec8596a2ae04
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-09 15:10:41 -0600

    minor

--- food_project/image_classification/ingredient_prediction_server.py
+++ food_project/image_classification/ingredient_prediction_server.py
@@ -37,6 +37,7 @@ def class_probabilities(np_arr):
     print(type(probs))
     pred_probs = {}
     for i in range(probs.shape[0]):
+        print(i)
         cls = reverse_map[i]
         cls = parse.unquote_plus(cls)
         pred_probs[cls] = probs[i]
