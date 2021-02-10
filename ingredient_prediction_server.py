5c78dd24dff8df144a1629ca1753dc6f7af21935
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-09 22:06:21 -0600

    minor

--- ingredient_prediction_server.py
+++ ingredient_prediction_server.py
@@ -56,7 +56,8 @@ def predict():
     image = np.array(Image.open(image), dtype=float)
 
     predictions = predict_image(image, image_classification_model=Classifier())
-    print(predictions)
+    import pdb
+    pdb.set_trace()
     crf = CRF(9, 5)
     for i in range(3):
         for j in range(3):
