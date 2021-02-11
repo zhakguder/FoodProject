285d706ee1a8755555ebd1dc963aaaac6d6cbca6
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-11 17:35:24 -0600

    debug

--- food_project/recipe/models/weight_models.py
+++ food_project/recipe/models/weight_models.py
@@ -72,7 +72,7 @@ class RecipeWeightIngredientModel(RecipeModel):
         missing_columns = set(tmp.columns) - set(tmp_df.columns)
         missing_col_dict = {k: 0 for k in missing_columns}
         tmp_df = tmp_df.assign(**missing_col_dict)
-        return df.reindex(sorted(tmp_df.columns), axis=1)
+        return tmp_df.reindex(sorted(tmp_df.columns), axis=1)
 
 
 class RecipeWeightClusterModel(RecipeWeightIngredientModel):
