fcd6179e484ee42d8fbd225a9b5cf7ae5a8fc870
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-13 15:32:41 -0600

    minor

--- food_project/recipe/crf/__init__.py
+++ food_project/recipe/crf/__init__.py
@@ -7,6 +7,7 @@ def get_recipe_counts_containing_ingredients(*ings):
     """Returns the number of recipes that contain both ing1 and ing2"""
 
     ingrs_df = cluster_df[list(ings)]
+    print(ingrs_df)
     res = sum(ingrs_df.all(axis=1))
     # res = sum(cluster_df.apply(lambda x: x[ings[0]] == 1 and x[ings[1]] == 1, axis=1))
     print(res)
