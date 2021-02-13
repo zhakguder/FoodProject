f1d4d3b8d6458d5a0b8c4795c098c85b0b264fca
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-13 15:23:40 -0600

    minor

--- food_project/recipe/crf/__init__.py
+++ food_project/recipe/crf/__init__.py
@@ -6,7 +6,7 @@ from food_project.recipe.crf.cluster_bi_occurrences import cluster_df
 def get_recipe_counts_containing_ingredients(*ings):
     """Returns the number of recipes that contain both ing1 and ing2"""
 
-    print(cluster_df[ings])
+    print(cluster_df[list(ings)])
     res = sum(cluster_df.apply(lambda x: x[ings[0]] == 1 and x[ings[1]] == 1, axis=1))
     return res
 
