1b01e340b00e49803d07b10abfc33723ed8fcd4d
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-14 09:42:57 -0600

    minor

--- food_project/recipe/crf/__init__.py
+++ food_project/recipe/crf/__init__.py
@@ -6,7 +6,7 @@ from food_project.recipe.crf.cluster_bi_occurrences import cluster_df
 def get_recipe_counts_containing_ingredients(*ings):
     """Returns the number of recipes that contain all ingredients in ings"""
     ingrs_df = cluster_df[list(ings)]
-    return ingrs_df.all(axis=1)
+    return sum(ingrs_df.all(axis=1))
 
 
 def get_number_of_recipes():
