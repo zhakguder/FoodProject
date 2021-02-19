2f60fd110a6fba2c4087d12fcde4ee53b767f76e
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2021-02-18 22:31:40 -0600

    minor fix

--- food_project/image_classification/crf/__init__.py
+++ food_project/image_classification/crf/__init__.py
@@ -3,15 +3,15 @@
 model. Ingredient model expects a grid made up of individual images."""
 
 
-import os
+# import os
 
-from food_project.image_classification.crf.preprocess import GridImageCollector
-from food_project.image_classification.crf.test import (make_test_image,
-                                                        read_individual_image)
+# from food_project.image_classification.crf.preprocess import GridImageCollector
+# from food_project.image_classification.crf.test import (make_test_image,
+#                                                         read_individual_image)
 
-test_raw_image_dir = "data/crf/test_images/raw/"
-compiled_image = make_test_image(n=3, k=4, test_image_dir=test_raw_image_dir)
-grid_image = GridImageCollector(compiled_image, 3, 3)
+# test_raw_image_dir = "data/crf/test_images/raw/"
+# compiled_image = make_test_image(n=3, k=4, test_image_dir=test_raw_image_dir)
+# grid_image = GridImageCollector(compiled_image, 3, 3)
 
-for path in os.listdir(test_raw_image_dir):
-    image = read_individual_image(test_raw_image_dir + path)
+# for path in os.listdir(test_raw_image_dir):
+#     image = read_individual_image(test_raw_image_dir + path)
